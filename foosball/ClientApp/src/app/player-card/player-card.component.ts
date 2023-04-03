fc5a5d3dc288248b034ca249f5f1aa4848088fc2
themmixproject <steven.devalk@gmail.com>
2023-04-03 11:46:19 +0200

    Imported player data from ngFor loop into player-card component, added player-card styling from player-manager component

--- foosball/ClientApp/src/app/player-card/player-card.component.ts
+++ foosball/ClientApp/src/app/player-card/player-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-player-card',
-  templateUrl: './player-card.component.html',
-  styleUrls: ['./player-card.component.css']
+    selector: 'app-player-card',
+    templateUrl: './player-card.component.html',
+    styleUrls: ['./player-card.component.css']
 })
 export class PlayerCardComponent {
-
+    @Input() player: any;
 }
