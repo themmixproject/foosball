01507b79ed983364b5591740c41559dd336f9736
themmixproject <steven.devalk@gmail.com>
2023-04-04 12:09:08 +0200

    Added post request on player creation

--- foosball/ClientApp/src/app/player-manager/player-manager.component.ts
+++ foosball/ClientApp/src/app/player-manager/player-manager.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { AlertService } from "../alert.service";
 import { PlayerService } from "../player.service";
 import { Player } from "src/player";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Inject } from "@angular/core";
 
 @Component({
     selector: "app-player-manager",
@@ -11,21 +13,38 @@ import { Player } from "src/player";
 export class PlayerManagerComponent implements OnInit {
     constructor(
         private alertService: AlertService,
-        private playerService: PlayerService
-    ) {}
+        private playerService: PlayerService,
+        private http: HttpClient,
+        @Inject("BASE_URL") baseUrl: string
+    ) {
+        this.baseUrl = baseUrl;
+    }
 
+    baseUrl: string = "";
     playerName: string = "";
     public players: Array<Player> = [];
     ngOnInit(): void {
         this.players = this.playerService.players;
+        this.playerName = "Timothy";
     }
 
+    private _options = {
+        headers: new HttpHeaders({ "Content-Type": "application/json" }),
+    };
     public createNewPlayer() {
         if (this.playerName === "") {
             this.alertService.toggleAlert("Player name cannot be empty.");
         } else {
             console.log("Create player: " + this.playerName);
             this.playerService.players.push(new Player(this.playerName));
+            this.http
+                .post(
+                    this.baseUrl + "player",
+                    JSON.stringify({ "name": this.playerName }),
+                    this._options
+                ).subscribe(function(result) {
+                    console.log(result);
+                });
         }
     }
 }
