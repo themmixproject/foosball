94895f52f1fcb0e887ac48fbc3c855362d16f43c
themmixproject <steven.devalk@gmail.com>
2023-04-05 18:07:27 +0200

    Added boilerplate for game-manager component, added game-manager to nav-menu

---
+++ foosball/ClientApp/src/app/game-manager/game-manager.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GameManagerComponent } from './game-manager.component';
+
+describe('GameManagerComponent', () => {
+  let component: GameManagerComponent;
+  let fixture: ComponentFixture<GameManagerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GameManagerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GameManagerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ foosball/ClientApp/src/app/game-manager/game-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-manager',
+  templateUrl: './game-manager.component.html',
+  styleUrls: ['./game-manager.component.css']
+})
+export class GameManagerComponent {
+
+}
