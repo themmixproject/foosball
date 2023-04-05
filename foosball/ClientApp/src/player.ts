770776d639d709a55ea055d345b8a98ccd8877b2
themmixproject <steven.devalk@gmail.com>
2023-04-05 12:18:32 +0200

    Added playerId to Player class

--- foosball/ClientApp/src/player.ts
+++ foosball/ClientApp/src/player.ts
@@ -1,9 +1,11 @@
 export class Player {
     constructor(name: string){
+        this.playerId = 0;
         this.name = name;
         this.score = 0;
     }
 
+    public playerId: number;
     public name: string;
     public score: number;
 }
