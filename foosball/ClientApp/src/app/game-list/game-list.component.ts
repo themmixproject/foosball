37948d33172058ae23ba91664e46e47c2a8d476d
themmixproject <steven.devalk@gmail.com>
2023-04-07 15:45:17 +0200

    Loaded global games list from game Service, bound game data to game list items

--- foosball/ClientApp/src/app/game-list/game-list.component.ts
+++ foosball/ClientApp/src/app/game-list/game-list.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import { GameService } from "../game.service";
 
 @Component({
     selector: "app-game-list",
     templateUrl: "./game-list.component.html",
     styleUrls: ["./game-list.component.css"],
 })
-export class GameListComponent {}
+export class GameListComponent {
+    constructor(public gameService: GameService) {}
+}
