f096c42d8c916043fea88fb5ef871bb548eacfc9
themmixproject <steven.devalk@gmail.com>
2023-04-07 15:27:32 +0200

    Added default player argument in Teams class

--- foosball/ClientApp/src/team.ts
+++ foosball/ClientApp/src/team.ts
@@ -1,13 +1,15 @@
 import { Player } from "./player";
 
 export class Team {
-    constructor(name: string){
+    constructor(name: string, players: Player[] = []){
         this.teamId = 0;
         this.name = name;
-        this.players = [];
+        this.score = 0;
+        this.players = players;
     }
 
     public teamId: number;
     public name: string;
+    public score: number;
     public players: Array<Player>;
 }
