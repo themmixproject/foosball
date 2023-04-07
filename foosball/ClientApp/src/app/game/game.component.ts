4f9e5df852cc4910fb509214d1309c36837f335c
themmixproject <steven.devalk@gmail.com>
2023-04-07 16:28:41 +0200

    Added styling and html binding to game component

--- foosball/ClientApp/src/app/game/game.component.ts
+++ foosball/ClientApp/src/app/game/game.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { GameService } from "../game.service";
+import { Game } from "src/game";
 
 @Component({
     selector: "app-game",
     templateUrl: "./game.component.html",
     styleUrls: ["./game.component.css"],
 })
-export class GameComponent {}
+export class GameComponent {
+    constructor(public gameService: GameService){}
+    game: Game = this.gameService.games[0];
+}
