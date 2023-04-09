6cee50a3f2544697f40d840dfdc55e261c52c01f
themmixproject <steven.devalk@gmail.com>
2023-04-09 21:59:02 +0200

    Added dummy data to game service, added game() method to handle increaseScore() and decreaseScore() methods

--- foosball/ClientApp/src/app/game.service.ts
+++ foosball/ClientApp/src/app/game.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { Game } from "src/game";
 import { Player } from "src/player";
 import { Team } from "src/team";
+import { TeamMember } from "src/team-member";
 
 @Injectable({
     providedIn: "root",
@@ -10,10 +11,38 @@ export class GameService {
     constructor() {
         this.games = [
             new Game([
-                new Team("Red", [new Player("Gregg"), new Player("Toby")]),
-                new Team("Blue", [new Player("Natasha"), new Player("Sophie")]),
+                new Team("Red", [
+                    new TeamMember(new Player("Gregg")),
+                    new TeamMember(new Player("Toby")),
+                ]),
+                new Team("Blue", [
+                    new TeamMember(new Player("Natasha")),
+                    new TeamMember(new Player("Sophie")),
+                ]),
             ]),
         ];
     }
+
     public games: Game[];
+
+    public game(index: number) {
+        let game: Game = this.games[index];
+        return {
+            increaseScore(teamIndex: number, teamMemberIndex: number) {
+                let team = game.teams[teamIndex];
+                team.score++;
+                team.members[teamMemberIndex].score++;
+                team.members[teamMemberIndex].player.score++;
+            },
+            decreaseScore(teamIndex: number, teamMemberIndex: number) {
+                let team = game.teams[teamIndex];
+                let teamMember = team.members[teamMemberIndex];
+                if (team.score > 0 && teamMember.score > 0) {
+                    team.score--;
+                    team.members[teamMemberIndex].score--;
+                    team.members[teamMemberIndex].player.score--;
+                }
+            },
+        };
+    }
 }
