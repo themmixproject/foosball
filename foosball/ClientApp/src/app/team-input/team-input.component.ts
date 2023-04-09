f5636a792dfabdf54c2783bbfbe33045d3a84e68
themmixproject <steven.devalk@gmail.com>
2023-04-09 22:57:14 +0200

    Added team name binding to team-input component

--- foosball/ClientApp/src/app/team-input/team-input.component.ts
+++ foosball/ClientApp/src/app/team-input/team-input.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { Team } from "src/team";
 
 @Component({
-  selector: 'app-team-input',
-  templateUrl: './team-input.component.html',
-  styleUrls: ['./team-input.component.css']
+    selector: "app-team-input",
+    templateUrl: "./team-input.component.html",
+    styleUrls: ["./team-input.component.css"],
 })
 export class TeamInputComponent {
-
+    constructor(){this.team = new Team("")}
+    @Input() team: Team;
 }
