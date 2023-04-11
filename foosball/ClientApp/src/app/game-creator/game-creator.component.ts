670750426822870ad9e5acf5fca5c8ccf2b4a9b0
themmixproject <steven.devalk@gmail.com>
2023-04-11 14:23:35 +0200

    Added playerSelectorTeam variable in game-creator component, added availablePlayers array to toggle hiding player from player selector

--- foosball/ClientApp/src/app/game-creator/game-creator.component.ts
+++ foosball/ClientApp/src/app/game-creator/game-creator.component.ts
@@ -10,17 +10,32 @@ import { PlayerService } from "../player.service";
     styleUrls: ["./game-creator.component.css"],
 })
 export class GameCreatorComponent {
-    constructor(private gameService: GameService, private playerService: PlayerService) {
+    constructor(
+        private gameService: GameService,
+        private playerService: PlayerService
+    ) {
         this.teams = [new Team("Blue"), new Team("Red")];
-        this._subscription_players = this.playerService.playersChange.subscribe((players) => {
-            this.players = players;
-        })
+        this._subscription_players = this.playerService.playersChange.subscribe(
+            (players) => {
+                this.players = players;
+                for (let i = 0; i < this.players.length; i++) {
+                    if (this.availablePlayers[i] !== undefined) {
+                        if (this.availablePlayers[i] === false) {
+                            this.availablePlayers[i] = false;
+                        }
+                    }
+                    this.availablePlayers[i] = true;
+                }
+            }
+        );
         this.players = this.playerService.players;
+        this.playerSelectorTeam = this.teams[0];
     }
 
     teams: Array<Team> = [];
-    players: Array<any> = []
-    _subscription_players : any;
+    players: Array<any> = [];
+    availablePlayers: Array<Boolean> = [];
+    _subscription_players: any;
 
     gameCreatorFormDisplay: Boolean = true;
     public toggleGameCreatorForm() {
@@ -28,11 +43,21 @@ export class GameCreatorComponent {
     }
 
     playerSelectorFormDisplay: Boolean = true;
-    public togglePlayerSelectorForm(){
+    public togglePlayerSelectorForm() {
         this.playerSelectorFormDisplay = !this.playerSelectorFormDisplay;
     }
 
-    public createGame(){
+    public createGame() {
         this.gameService.createGame(this.teams);
+    }
+
+    public addPlayerToTeam(playerIndex: number) {
+        this.availablePlayers[playerIndex] = false;
+        console.log(this.availablePlayers[playerIndex]);
+    }
+
+    public playerSelectorTeam: Team;
+    public setPlayerSelectorTeam(teamIndex: number) {
+        this.playerSelectorTeam = this.teams[teamIndex];
     }
 }
