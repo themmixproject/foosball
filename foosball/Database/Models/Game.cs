dffdca550b96fff8941a3994b0730c118f4505a9
themmixproject <steven.devalk@gmail.com>
2023-04-11 21:00:02 +0200

    Deleted TeamScores property from Game, added constructor to prevent Teams being null

--- foosball/Database/Models/Game.cs
+++ foosball/Database/Models/Game.cs
@@ -2,6 +2,9 @@
     public class Game {
         public int GameId { get; set; }
         public ICollection<Team> Teams { get; set; }
-        public List<int> TeamScores { get; set; }
+
+        public Game(ICollection<Team> teams) {
+            Teams = teams;
+        }
     }
 }
