5da0a8f24e4ff6ea857b3c08b1d61af459d9cf75
themmixproject <steven.devalk@gmail.com>
2023-04-11 20:48:35 +0200

    Deleted score list from game.ts class

--- foosball/ClientApp/src/game.ts
+++ foosball/ClientApp/src/game.ts
@@ -3,9 +3,7 @@ import { Team } from "./team";
 export class Game {
     constructor(teams : Team[]){
         this.teams = teams;
-        this.scores = [0 ,0]
     }
 
     public teams : Team[];
-    public scores: number[];
 }
