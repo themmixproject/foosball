3627ea6c04a56deb5852176fd71d846db0c2fc54
themmixproject <steven.devalk@gmail.com>
2023-04-13 01:30:21 +0200

    Added migration for adding Name and Score properties to Teams model

---
+++ foosball/Migrations/202304122329507_Added Name and Score properties to Teams model.cs
@@ -0,0 +1,20 @@
+﻿namespace foosball.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNameandScorepropertiestoTeamsmodel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Teams", "Name", c => c.String());
+            AddColumn("dbo.Teams", "Score", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Teams", "Score");
+            DropColumn("dbo.Teams", "Name");
+        }
+    }
+}
