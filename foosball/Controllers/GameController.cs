633e7ba9af21aef5f96462ecf25e4960f8187b6f
themmixproject <steven.devalk@gmail.com>
2023-04-12 19:15:37 +0200

    Added GameController with GET and POST request

---
+++ foosball/Controllers/GameController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using foosball.Database.Models;
+using foosball.Database;
+using System.Data.Entity;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace foosball.Controllers {
+    [Route( "api/[controller]" )]
+    [ApiController]
+    public class GameController : ControllerBase {
+
+        public readonly FoosballContext foosballContext = new FoosballContext();
+
+        // GET: api/<GameController>
+        [HttpGet]
+        public IEnumerable<Game> Get() {
+            return foosballContext.Games.ToList();
+        }
+
+        // GET api/<GameController>/5
+        //[HttpGet( "{id}" )]
+        //public string Get( int id ) {
+        //    return "value";
+        //}
+
+        // POST api/<GameController>
+        [HttpPost]
+        public IActionResult Post(List<Team> teams){
+            
+            foosballContext.Games.Add(new Game(teams));
+            foreach(Team team in teams) {
+                foreach(TeamMember member in team.Members) {
+                    foosballContext.Entry<Player>(member.Player).State = EntityState.Unchanged;
+                }
+            }
+
+            foosballContext.SaveChanges();
+            return Ok();
+        }
+
+        // PUT api/<GameController>/5
+        //[HttpPut( "{id}" )]
+        //public void Put( int id, [FromBody] string value ) {
+        //}
+
+        //// DELETE api/<GameController>/5
+        //[HttpDelete( "{id}" )]
+        //public void Delete( int id ) {
+        //}
+    }
+}
