f99a2e4281d01f7651089a496d70f805af878ea7
themmixproject <steven.devalk@gmail.com>
2023-04-13 01:58:48 +0200

    Added gameId property to Game class

--- foosball/ClientApp/src/game.ts
+++ foosball/ClientApp/src/game.ts
@@ -3,7 +3,9 @@ import { Team } from "./team";
 export class Game {
     constructor(teams : Team[]){
         this.teams = teams;
+        this.gameId  = 0;
     }
 
+    public gameId : number;
     public teams : Team[];
 }
