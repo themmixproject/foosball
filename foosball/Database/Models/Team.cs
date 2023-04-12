6be602f0436a57e17b444b5d5743a6c8c2d960fe
themmixproject <steven.devalk@gmail.com>
2023-04-13 01:29:07 +0200

    Added Name and Score properties to Teams model

--- foosball/Database/Models/Team.cs
+++ foosball/Database/Models/Team.cs
@@ -1,6 +1,8 @@
 ﻿namespace foosball.Database.Models {
     public class Team {
         public int TeamId { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
         public virtual ICollection<TeamMember> Members { get; set; }
         public Team(ICollection<TeamMember> members) {
             Members = members;
