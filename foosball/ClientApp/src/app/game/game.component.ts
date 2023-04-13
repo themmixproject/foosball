2feed18e01d8acaa7a385480b42a5e170364bb8a
themmixproject <steven.devalk@gmail.com>
2023-04-13 22:14:23 +0200

    Added parsing of id parameter in game component, added subscription to game changes

--- foosball/ClientApp/src/app/game/game.component.ts
+++ foosball/ClientApp/src/app/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Game } from "src/game";
 import { GameService } from "../game.service";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "app-game",
@@ -8,9 +9,29 @@ import { GameService } from "../game.service";
     styleUrls: ["./game.component.css"],
 })
 export class GameComponent {
-    constructor(public gameService: GameService) {}
+    constructor(public gameService: GameService, private route: ActivatedRoute) {
+        this.gameId = 0;
+        let idParam = this.route.snapshot.queryParamMap.get("id");
+        if(idParam!=null){
+            this.gameId = 1
+        }
+        else {
+            if(idParam!=null){
+                this.gameId = idParam;
+            }
+        }
+        this.game = new Game([]);
+        this._subscription_games = this.gameService.execChange.subscribe((games) => {
+            this.game = gameService.getGameById(this.gameId);
+        })
+        this.game = this.gameService.getGameById(this.gameId);
+    }
     
-    game: Game = this.gameService.games[0];
+    // game: Game = this.gameService.games[0];
+    public gameId: number;
+    
+    game: Game;
+    _subscription_games :any;
 
     public increaseScore(teamIndex: number, playerIndex: number) {
         this.gameService.game(0).increaseScore(teamIndex, playerIndex);
