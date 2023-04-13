49e911c37cf3b9a58b005cfe52d9f372c3484995
themmixproject <steven.devalk@gmail.com>
2023-04-13 23:05:08 +0200

    Added teamMemberId property to team-member.ts

--- foosball/ClientApp/src/team-member.ts
+++ foosball/ClientApp/src/team-member.ts
@@ -4,8 +4,10 @@ export class TeamMember {
     constructor(player: Player) {
         this.player = player;
         this.score = 0;
+        this.teamMemberId = 0;
     }
 
     public player: Player;
     public score: number;
+    public teamMemberId: number;
 }
