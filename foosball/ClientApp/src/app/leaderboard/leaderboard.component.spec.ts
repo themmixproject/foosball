2ef9e7d6b0380087cdec9491c91242d637246970
themmixproject <steven.devalk@gmail.com>
2023-04-14 12:38:30 +0200

    Formatted leaderboard component files

--- foosball/ClientApp/src/app/leaderboard/leaderboard.component.spec.ts
+++ foosball/ClientApp/src/app/leaderboard/leaderboard.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { LeaderboardComponent } from './leaderboard.component';
+import { LeaderboardComponent } from "./leaderboard.component";
 
-describe('LeaderboardComponent', () => {
-  let component: LeaderboardComponent;
-  let fixture: ComponentFixture<LeaderboardComponent>;
+describe("LeaderboardComponent", () => {
+    let component: LeaderboardComponent;
+    let fixture: ComponentFixture<LeaderboardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LeaderboardComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LeaderboardComponent],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(LeaderboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(LeaderboardComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
