f84c0d481299821f7c6789a79945849d876e3cf6
themmixproject <steven.devalk@gmail.com>
2023-04-14 13:59:48 +0200

    Added data binding to leaderboard, added leaderboard styling

--- foosball/ClientApp/src/app/leaderboard/leaderboard.component.ts
+++ foosball/ClientApp/src/app/leaderboard/leaderboard.component.ts
@@ -1,8 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Player } from "src/player";
+import { PlayerService } from "../player.service";
 
 @Component({
     selector: "app-leaderboard",
     templateUrl: "./leaderboard.component.html",
     styleUrls: ["./leaderboard.component.css"],
 })
-export class LeaderboardComponent {}
+export class LeaderboardComponent implements OnInit{
+    constructor(private playerService: PlayerService){
+        this.playerService.getBestPlayers();
+        this._subscription_best_players = this.playerService.bestPlayersChange.subscribe((players) => {
+            this.players = players;
+        })
+    }
+
+    ngOnInit(): void {
+        this.players = this.playerService.bestPlayers;
+    }
+
+    public players: Player[] = [];
+    _subscription_best_players: any;
+}
